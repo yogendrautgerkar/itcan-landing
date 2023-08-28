714f1a88c4bac95c356dada18cb03925f686b9ac
YogendraTech <yogendrautgerkar@gmail.com>
2023-08-28 09:25:12 +0530

    Initial commit

---
+++ src/components/particles/DataLists.tsx
@@ -0,0 +1,241 @@
+export const NavLinks = [
+    {
+        name: "About",
+        url: "/"
+    },
+    {
+        name: "Services",
+        url: "/"
+    },
+    {
+        name: "Work",
+        url: "/"
+    },
+    {
+        name: "Blogs",
+        url: "/"
+    },
+    {
+        name: "Career",
+        url: "/"
+    }
+
+];
+
+export const NavButtons = [
+    {
+        name: "Login",
+        url: "/"
+    },
+    {
+        name: "Contact",
+        url: "/"
+    }
+
+];
+
+
+export const HeroTexts = {
+    firstText: "ITCAN Technology & Digital marketing",
+    secondText: "Empowering Technology",
+    thirdText: "As a full-service technology and digital marketing company we bring our insights, knowledge, and expertise to the digital ecosystem to create exponential growth for online retailers and e-commerces in the GCC region.",
+    firstButton: "Find out more",
+    secondButton: 'Play Demo'
+}
+
+
+export const ServiceTexts = {
+    firstText: "CATEGORY",
+    secondText: "We Offer Best Services",
+    cards: [
+        {
+            firstText: "Performance Marketing",
+            secondText: "We translate retail experiences into the digital ecosystem."
+        },
+        {
+            firstText: "Digital Marketing",
+            secondText: "We translate retail experiences into the digital ecosystem."
+        },
+        {
+            firstText: "Influencer Marketing",
+            secondText: "We translate retail experiences into the digital ecosystem."
+        },
+        {
+            firstText: "Technology Development",
+            secondText: "We translate retail experiences into the digital ecosystem."
+        }
+    ]
+}
+
+export const TopDestinationTexts = {
+    firstText: "Top selling",
+    secondText: "Top Destinations",
+    cards: [
+        {
+            country: "Rome, Italy",
+            price: "5.42k",
+            duration: "10 days trip"
+        },
+        {
+            country: "London, UK",
+            price: "4.2k",
+            duration: "12 days trip"
+        },
+        {
+            country: "Paris, France",
+            price: "15k",
+            duration: "26 days trip"
+        },
+        {
+            country: "New York, USA",
+            price: "4.5k",
+            duration: "8 days trip"
+        },
+        {
+            country: "Warsaw, Poland",
+            price: "3.9k",
+            duration: "14 days trip"
+        },
+        {
+            country: "Dubai, UAE",
+            price: "9.2k",
+            duration: "17 days trip"
+        },
+    ]
+}
+
+export const BookingStepsTexts = {
+    firstText: "OUR SERVICES",
+    secondText: "Technology Development",
+    listOfSteps: [
+        {
+            text: "We translate retail experiences into the digital ecosystem by building performance focused applications."
+        },
+        {
+            text: "We focus on enhancing the customer journey and creating meaningful interactions."
+        },
+        {
+            text: "We focus on enhancing the customer journey and creating meaningful interactions."
+        }
+    ],
+    cardOne:
+    {
+        name: "Trip To Greece",
+        date: "14-29 June  |  by Robbin Jobs",
+        people: "24 people going"
+    },
+    cardTwo: {
+        status: "Ongoing",
+        destination: "Trip to Rome",
+        completion: "40% Completed"
+    }
+
+}
+
+export const TestimonialTexts = {
+    firstText: "TESTIMONIALS",
+    secondText: "What people say about Us.",
+    feedBacks: [
+        {
+            text: "You did a fabulous job. There were no hitches. Thank you again for all your work planning this trip.",
+            person: "Mike taylor",
+            location: "Lahore, Pakistan"
+        },
+        {
+            text: "Jadoo was great with the entire process from planning to updates during the trip. We had 11 people and everything was perfectly executed. We appreciate all of her hard work. It was truly the trip of a lifetime. Thank you!",
+            person: "Gina",
+            location: "Owerri, Nigeria"
+        },
+        {
+            text: "Booking through you was very easy and made our lives so much easier. I have nothing bad to say! Thank you for giving us tips and guidance before we left on what to bring and such, that was very helpful!",
+            person: "Emma",
+            location: "Vancouver, Canada"
+        },
+        {
+            text: "Thank you for your recommendation and putting the trip together. The resort was absolutely beautiful. The infinity pools, the palm trees in the main pool, the infinity pool in out preferred area overlooking the gulf and the golf course were exceptional....",
+            person: "Loveth",
+            location: "Lagos, Nigeria"
+        },
+    ],
+}
+
+export const NewsletterTexts = {
+    firstText: "Subscribe to get information, latest news and other interesting offers about Jadoo.",
+    placeholderText: "Your email",
+    buttonText: "Subscribe"
+}
+
+export const FooterTexts = {
+    underLogoText: "Technology & Performance Marketing",
+    quickLinks: {
+        caption: "Quick Links",
+        links: [
+            {
+                name: "Destination",
+                url: "/"
+            },
+            {
+                name: "Hotels",
+                url: "/"
+            },
+            {
+                name: "Flights",
+                url: "/"
+            },
+            {
+                name: "Bookings",
+                url: "/"
+            },
+            {
+                name: "Login",
+                url: "/"
+            },
+            {
+                name: "Signup",
+                url: "/"
+            }
+        ]
+    },
+    contacts: {
+        caption: "Contacts",
+        links: [
+            {
+                name: "FAQs",
+                url: "/"
+            },
+            {
+                name: "Help",
+                url: "/"
+            },
+            {
+                name: "Policies",
+                url: "/"
+            },
+            {
+                name: "Terms & Conditions",
+                url: "/"
+            }
+        ]
+    },
+    more: {
+        caption: "More",
+        links: [
+            {
+                name: "Career",
+                url: "/"
+            },
+            {
+                name: "Airlines",
+                url: "/"
+            },
+            {
+                name: "Airline Fees",
+                url: "/"
+            },
+            {
+                name: "Low Fare Tips",
+                url: "/"
+            }
+        ]
+    }
+}
